518a55d07d09f3e35fd8c56ae025320e04717f4f
mitsui3point <rladbsgh0331@gmail.com>
2025-09-25 12:51:10 +0900

    feat(Pin): modify Pin - PinCount -> Pin

--- src/main/java/com/kata/bowling/Pin.java
+++ src/main/java/com/kata/bowling/Pin.java
@@ -3,20 +3,40 @@ package com.kata.bowling;
 import java.util.Objects;
 
 public class Pin {
+    public static final String EXCEED_PIN_COUNT = "exceed pin";
+    public static final String BELOW_PIN_COUNT = "below pin";
+    public static final int MAX_VALUE = 10;
+    public static final int MIN_VALUE = 0;
+    static final String NOT_ALLOWED_EXCEED_REST_PIN = "not allowed exceed rest pin";
+    private final int stand;
+    private final int fallen;
 
-    private final PinCount pinCount;
-
-    public Pin(PinCount pinCount) {
-        this.pinCount = pinCount;
+    private Pin(int stand) {
+        this.stand = stand;
+        this.fallen = MAX_VALUE - stand;
     }
 
-    public static Pin of(int stand) {
-        return new Pin(PinCount.of(stand));
+    public static Pin of(int rest) {
+        if (rest > MAX_VALUE) {
+            throw new IllegalArgumentException(EXCEED_PIN_COUNT);
+        }
+        if (rest < MIN_VALUE) {
+            throw new IllegalArgumentException(BELOW_PIN_COUNT);
+        }
+        return new Pin(rest);
     }
 
-    public Pin breakDown(int breakDown) {
-        PinCount breakDownPin = PinCount.of(breakDown);
-        return new Pin(this.pinCount.minus(breakDownPin));
+    public static Pin of() {
+        return new Pin(MAX_VALUE);
+    }
+
+    public Pin breakDown(int breakDownValue) {
+        Pin breakDown = Pin.of(breakDownValue);
+        if (this.stand < breakDown.stand) {
+            throw new IllegalArgumentException(NOT_ALLOWED_EXCEED_REST_PIN);
+        }
+
+        return new Pin(this.stand - breakDown.stand);
     }
 
     @Override
@@ -24,11 +44,11 @@ public class Pin {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Pin pin = (Pin) o;
-        return Objects.equals(pinCount, pin.pinCount);
+        return stand == pin.stand && fallen == pin.fallen;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(pinCount);
+        return Objects.hash(stand, fallen);
     }
 }
--- src/test/java/com/kata/bowling/PinTest.java
+++ src/test/java/com/kata/bowling/PinTest.java
@@ -1,24 +1,67 @@
 package com.kata.bowling;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static com.kata.bowling.Pin.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PinTest {
 
     @Test
     void create() {
-        Pin actual = Pin.of(10);
+        Pin actual = Pin.of(1);
+        Pin expected = Pin.of(1);
+
+        assertThat(actual).isEqualTo(expected);
+
+    }
+
+    @Test
+    void create_default() {
+        Pin actual = Pin.of();
         Pin expected = Pin.of(10);
 
         assertThat(actual).isEqualTo(expected);
     }
 
+    // 범위 제한
     @Test
-    void breakDown() {
-        Pin actual = Pin.of(10).breakDown(8);
-        Pin expected = Pin.of(2);
+    void create_fail_range() {
+        assertThatThrownBy(() -> Pin.of(11))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(EXCEED_PIN_COUNT);
+        assertThatThrownBy(() -> Pin.of(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(BELOW_PIN_COUNT);
+    }
+
+    // breakDown
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1, 9", "2, 8", "3, 7", "4, 6",
+            "5, 5",
+            "6, 4", "7, 3", "8, 2", "9, 1",
+    })
+    void breakDown(int minus, int result) {
+        Pin actual = Pin.of().breakDown(minus);
+        Pin expected = Pin.of(result);
 
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void breakDown_fail_range() {
+        assertThatThrownBy(() -> Pin.of(8).breakDown(9))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_ALLOWED_EXCEED_REST_PIN);
+        assertThatThrownBy(() -> Pin.of().breakDown(11))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(EXCEED_PIN_COUNT);
+        assertThatThrownBy(() -> Pin.of().breakDown(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(BELOW_PIN_COUNT);
     }
 }
